75e41d1fee1e06e5ad26bc88e376a68ea7f6975e
Fede <fede.atzeni372@gmail.com>
2025-01-09 17:26:17 +0100

    feat script conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,25 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function vowels(wordInput) {
+
+    let arrayVowels = []
+    let letters = wordInput.split("");
+
+    for (let i = 0; i < letters.length; i++) {
+        if (letters[i] === "a" || letters[i] === "e" || letters[i] === "i" || letters[i] === "o" || letters[i] === "u") {
+            arrayVowels.push(letters[i])
+        }
+
+    }
+
+    return arrayVowels
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(vowels(word));
+
 
 
 
